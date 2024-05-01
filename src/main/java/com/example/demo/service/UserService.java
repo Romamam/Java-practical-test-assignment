1ec5e538638c2e8b3649917bf2563e41e38668ed
roman <roman.ribachok@gmail.com>
2024-05-01 22:54:02 +0300

    created feature to update one or some fields and created custom exception

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,14 +1,22 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
+import com.example.demo.util.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 @Service
 public class UserService {
+
+    @Value("${user.min.age}")
+    private int minAge;
 
     private List<User> users = new ArrayList<>();
 
@@ -17,5 +25,75 @@ public class UserService {
         users.add(new User("user1@exasdasdample.com", "Joasdhasdn", "Dasasddoe","30.04.2003"));
         users.add(new User("user1@exasasddample.com", "Joasdhn", "Dasasasdddoe","30.04.2004"));
         users.add(new User("user1@exasasddample.com", "Joasdhn", "Daasdsdoe","30.04.2005"));
+    }
+
+    public List<User> getAllUsers(){
+        return users;
+    }
+
+    public User createUser(User user){
+        if(calculateAge(user) >= minAge){
+            users.add(user);
+        }
+        return null;
+    }
+
+    public User updateOneOrSomeUserFields(UUID id, String email, String firstName, String lastName, String birthDate,
+                                 String address, Integer phoneNumber) throws UserNotFoundException {
+        User userToUpdate = getUserById(id);
+
+        if (userToUpdate != null) {
+            if (email != null) {
+                userToUpdate.setEmail(email);
+            }
+            if (firstName != null) {
+                userToUpdate.setFirstName(firstName);
+            }
+            if (lastName != null) {
+                userToUpdate.setLastName(lastName);
+            }
+            if (birthDate != null) {
+                if (calculateAge(userToUpdate) < 18) {
+                    throw new IllegalArgumentException("User must be at least 18 years old.");
+                }
+                userToUpdate.setBirthDate(birthDate);
+            }
+            if (address != null) {
+                userToUpdate.setAddress(address);
+            }
+            if (phoneNumber != null) {
+                userToUpdate.setPhoneNumber(phoneNumber);
+            }
+
+            return userToUpdate;
+        } else {
+            throw new UserNotFoundException("User with ID " + " not found");
+        }
+    }
+
+    public User getUserById(UUID id) {
+        return users.stream()
+                .filter(user -> id.equals(user.getId()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int calculateAge(User user) {
+        LocalDate birthDate = parseToDate(user.getBirthDate());
+        LocalDate currentDate = LocalDate.now();
+
+        int age = currentDate.getYear() - birthDate.getYear();
+
+        if (birthDate.getMonthValue() > currentDate.getMonthValue() ||
+                (birthDate.getMonthValue() == currentDate.getMonthValue() && birthDate.getDayOfMonth() > currentDate.getDayOfMonth())) {
+            age--;
+        }
+
+        return age;
+    }
+
+    public LocalDate parseToDate(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return LocalDate.parse(date, formatter);
     }
 }
