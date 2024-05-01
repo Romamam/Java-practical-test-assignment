a8b5c9e3d84abf0cd7013647324ae09d42d80119
roman <roman.ribachok@gmail.com>
2024-05-01 22:49:47 +0300

    added logics for get all users and create user(more than 18 y.o.)

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import com.example.demo.util.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable UUID id) throws UserNotFoundException {
+        User user = userService.getUserById(id);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        } else {
+            throw new UserNotFoundException("Uesr with ID " + id + " not found");
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateOneOrSomeUserFields(@PathVariable UUID id,
+                                                            @RequestBody User user) {
+        try {
+            User updatedUser = userService.updateUserFields(id,
+                    user.getEmail(),
+                    user.getFirstName(),
+                    user.getLastName(),
+                    user.getBirthDate(),
+                    user.getAddress(),
+                    user.getPhoneNumber());
+            return ResponseEntity.ok(updatedUser);
+        } catch (UserNotFoundException ex) {
+            String errorMessage = "User with ID " + id + " not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
+    }
+
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
+        String errorMessage = "User not found: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
