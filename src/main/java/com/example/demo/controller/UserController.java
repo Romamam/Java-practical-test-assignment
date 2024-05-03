30a8364e8aa9fedc868054f1cf3f0c88eac6bed4
roman <roman.ribachok@gmail.com>
2024-05-03 18:17:08 +0300

    added field id and added validation for fields

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import com.example.demo.util.PartialUserUpdateRequest;
-import com.example.demo.util.UserNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User newUser = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
@@ -45,7 +46,7 @@ public class UserController {
 
     @PatchMapping("/users/{id}")
     public ResponseEntity<Object> updateOneOrSomeUserFields(@PathVariable UUID id,
-                                                            @RequestBody PartialUserUpdateRequest request) {
+                                                            @Valid @RequestBody PartialUserUpdateRequest request) {
         try {
             User updatedUser = userService.updateOneOrSomeUserFields(id, request);
             return ResponseEntity.ok(updatedUser);
@@ -56,7 +57,7 @@ public class UserController {
     }
 
     @PutMapping("/users/{id}")
-    public ResponseEntity<Object> updateAllUserFields(@PathVariable UUID id, @RequestBody User updatedUserFields){
+    public ResponseEntity<Object> updateAllUserFields(@PathVariable UUID id, @Valid @RequestBody User updatedUserFields){
         try{
             User updatedUser = userService.updateAllUserFields(id, updatedUserFields);
             return ResponseEntity.ok(updatedUser);
@@ -81,11 +82,5 @@ public class UserController {
         } catch (IllegalArgumentException ex) {
             return ResponseEntity.badRequest().body(Collections.emptyList());
         }
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
-        String errorMessage = "User not found: " + ex.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
     }
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,10 @@
 package com.example.demo.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,21 +16,30 @@ import java.util.UUID;
 public class User {
 
     private UUID id;
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
+    @NotEmpty
+    @NotNull
     private String firstName;
+    @NotEmpty
+    @NotNull
     private String lastName;
+    @NotEmpty
+    @NotNull
+    @Pattern(regexp = "^\\d{2}\\.\\d{2}\\.\\d{4}$", message = "Birth date must be in the format dd.MM.yyyy")
     private String birthDate;
     private String address;
     private int phoneNumber;
 
-    public User(String email, String firstName, String lastName, String birthDate){
+    public User(UUID id, String email, String firstName, String lastName, String birthDate){
         this.id = UUID.randomUUID();
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
     }
-    public User(String email, String firstName, String lastName, String birthDate, String address){
+    public User(UUID id, String email, String firstName, String lastName, String birthDate, String address){
         this.id = UUID.randomUUID();
         this.email = email;
         this.firstName = firstName;
@@ -33,7 +47,7 @@ public class User {
         this.birthDate = birthDate;
         this.address = address;
     }
-    public User(String email, String firstName, String lastName, String birthDate, int phoneNumber){
+    public User(UUID id, String email, String firstName, String lastName, String birthDate, int phoneNumber){
         this.id = UUID.randomUUID();
         this.email = email;
         this.firstName = firstName;
