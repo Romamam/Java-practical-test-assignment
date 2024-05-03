41bf85624a44d9340e22dc7c3e3759c51105da3d
roman <roman.ribachok@gmail.com>
2024-05-03 18:22:02 +0300

    added field validation

--- src/main/java/com/example/demo/util/PartialUserUpdateRequest.java
+++ src/main/java/com/example/demo/util/PartialUserUpdateRequest.java
@@ -1,12 +1,24 @@
 package com.example.demo.util;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PartialUserUpdateRequest {
+
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
+    @NotEmpty
+    @NotNull
     private String firstName;
+    @NotEmpty
+    @NotNull
     private String lastName;
+    @NotEmpty
+    @NotNull
     private String birthDate;
     private String address;
     private Integer phoneNumber;
