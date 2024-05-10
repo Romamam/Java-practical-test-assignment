20fd4dbb771fc2843ffaf56292c57c85d1f76a0c
roman <roman.ribachok@gmail.com>
2024-05-11 02:36:43 +0300

    added pattern to birth date

--- src/main/java/com/example/demo/util/PartialUserUpdateRequest.java
+++ src/main/java/com/example/demo/util/PartialUserUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.util;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -10,6 +11,7 @@ public class PartialUserUpdateRequest {
     private String email;
     private String firstName;
     private String lastName;
+    @Pattern(regexp = "^\\d{2}\\.\\d{2}\\.\\d{4}$", message = "Birth date must be in the format dd.MM.yyyy")
     private String birthDate;
     private String address;
     private Integer phoneNumber;
